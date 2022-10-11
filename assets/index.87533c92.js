25a63bf77fde7b225c3db6fc1abf59a76207020e
JinTengHuaJin <13693220689@163.com>
2022-10-11 16:59:05 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 928574869453362224d4e7488425452a4e53da4d

---
+++ assets/index.87533c92.js
@@ -0,0 +1,1 @@
+import{d as o,c as e,r as n}from"./index.7533fbb1.js";const d=o({name:"HelloWorld",setup(r,{emit:s,slots:t}){return()=>e("div",{class:"workbench-section"},[e("div",{class:"header-editor-container"},[e(n("headerUse"),null,null)])])}});export{d as default};
