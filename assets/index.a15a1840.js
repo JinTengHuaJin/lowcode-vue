634feac054a2c3e63742d7560a3369838706663a
JinTengHuaJin <13693220689@163.com>
2022-10-18 14:20:52 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 80d062e0ae6574a4166a32561264f0783d803bde

---
+++ assets/index.a15a1840.js
@@ -0,0 +1,1 @@
+import{d as t,c as e}from"./index.9438c1a8.js";const r=t({name:"antv",setup(s,{emit:n,slots:a}){return()=>e("div",{class:"strategic-canvas"},[e("section",{class:"left"},null)])}});export{r as default};
